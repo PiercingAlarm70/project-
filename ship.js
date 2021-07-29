9f7fff9976a8757ba988ab1ec9fd134bcaf98208
PiercingAlarm70 <86883494+PiercingAlarm70@users.noreply.github.com>
2021-07-29 19:32:07 +0100

    Add files via upload

---
+++ ship.js
@@ -0,0 +1,25 @@
+var shipImg1
+var shipImg2
+var sea
+
+var ship = createsprite = ship 
+
+ship = Loadanimation = shipImg1, shipImg2
+
+function preload(){
+
+  var shipImg1 = Loadanimation = ship-1.png
+var shipImg2 = Loadanimation = ship-2.png
+var sea = Loadanimation = sea.png
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+  if( background.x < 0){ background.x = background.width/8; }
+}
